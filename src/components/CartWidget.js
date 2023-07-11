3913c5f6cac2711277fd1e66e74f95a29cd02688
maurois <mauroschwab@gmail.com>
2023-07-11 19:36:35 -0300

    cart badge

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -1,14 +1,15 @@
-import { IconButton, colors } from "@mui/material";
+import { Badge, IconButton, colors } from "@mui/material";
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 
+
 const CartWidget = () => {
   return ( 
-    <>
-      <IconButton>
+    <Badge badgeContent={4} color="secondary">
+    <IconButton>
         <ShoppingCartOutlinedIcon sx={{color:"white"}}/>
       </IconButton>
-    </> 
+  </Badge>
   );
 }
  
